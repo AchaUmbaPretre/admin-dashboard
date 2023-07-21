c551948134fdb5571df757f1e1f5ba6d6fe33fcc
AchaUmbaPretre <achandambi@gmail.com>
2023-07-21 13:45:27 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { ColorModeContext, useMode} from './scenes/theme';
-import { CssBaseline, ThemeProvider  } from '@mui/material';
-import Topbar from './scenes/global/Topbar';
-import Dashboard from './scenes/dashboard'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebars from "./scenes/global/Sidebars";
+import Dashboard from "./scenes/dashboard";
 /* import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
@@ -10,29 +11,26 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+import Geography from "./scenes/geography"; */
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar"; */
-import { Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
-import Sidebars from './scenes/global/Sidebars';
-
+import { ColorModeContext, useMode } from "./scenes/theme";
+/* import Calendar from "./scenes/calendar/calendar"; */
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
+        <CssBaseline />
         <div className="app">
           <Sidebars isSidebar={isSidebar} />
           <main className="content">
-            <Topbar/>
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard/>}/>
-  {/*             <Route path="/team" element={<Team />} />
+              <Route path="/" element={<Dashboard />} />
+{/*               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,4 +1,4 @@
-import { tokens } from "../theme";
+import { tokens } from "../scenes/theme";
 
 export const mockDataTeam = [
   {
